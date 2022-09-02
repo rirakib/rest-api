0fb833d4d093755bb6b78657c2782b3a4b795296
rirakib <rirrakib10@gmail.com>
2022-09-02 17:27:05 +0600

    latest

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = new User();
+        $user->name = "shanto";
+        $user->email = "shanto@gmail.com";
+        $user->password = "123456";
+        $user->save();
+        
+    }
+}
